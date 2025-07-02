bbbb59c06965759a1f572013c8a1dea4966d337f
Швец Илья <ilyashvets2019@mail.ru>
2025-07-02 20:45:30 +0300

    Начало работы

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,7 +3,6 @@
 #include "cmdline.hpp"
 #include "verilog/Verilog.hpp"
 #include "verilog/VerilogReader.hpp"
-#include "lef/LEF.hpp"
 
 
 int main(int argc, char *argv[]) {
@@ -15,8 +14,6 @@ int main(int argc, char *argv[]) {
     VerilogReader   verilogReader;
     if (!verilogReader.read(cmdLine.verilog, netlist))
         return EXIT_FAILURE;
-
-    LEF_READER lef_reader;
 
     return EXIT_SUCCESS;
 
